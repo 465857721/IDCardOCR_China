52917baaeb9cd07a834e230fd745a9a9ff99032b
周康 <465857721@qq.com>
2016-12-15 10:03:05 +0800

    Initial commit

---
+++ app/src/main/java/com/kingsoft/idcardocr_china/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kingsoft.idcardocr_china;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
